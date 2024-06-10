7e0e8711c50ae21984c0ef60eb45490948258608
Shreyansh Singh <117681755+shreyansh7082@users.noreply.github.com>
2024-06-10 23:10:58 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Nuts and Bolts Problem/nuts-and-bolts-problem.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            char[] nuts = new char[n], bolts = new char[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                nuts[i] = (inputLine[i].charAt(0));
+            }
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                bolts[i] = (inputLine[i].charAt(0));
+            }
+
+            new Solution().matchPairs(n, nuts, bolts);
+            for (int i = 0; i < n; i++) {
+                System.out.print(nuts[i] + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < n; i++) {
+                System.out.print(bolts[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Ja
+
+class Solution {
+    private static final char[] order = { '!', '#', '$', '%', '&', '*', '?', '@', '^' };
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        matchPairsUtil(nuts, bolts, 0, n - 1);
+    }
+    private void matchPairsUtil(char[] nuts, char[] bolts, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(nuts, low, high, bolts[high]);
+            partition(bolts, low, high, nuts[pivotIndex]);
+            matchPairsUtil(nuts, bolts, low, pivotIndex - 1);
+            matchPairsUtil(nuts, bolts, pivotIndex + 1, high);
+        }
+    }
+    private int partition(char[] array, int low, int high, char pivot) {
+        int i = low;
+        for (int j = low; j < high; j++) {
+            if (compare(array[j], pivot) < 0) {
+                swap(array, i, j);
+                i++;
+            } else if (compare(array[j], pivot) == 0) {
+                swap(array, j, high);
+                j--;
+            }
+        }
+        swap(array, i, high);
+        return i;
+    }
+    private int compare(char a, char b) {
+        int indexA = -1, indexB = -1;
+        for (int i = 0; i < order.length; i++) {
+            if (order[i] == a) {
+                indexA = i;
+            }
+            if (order[i] == b) {
+                indexB = i;
+            }
+        }
+        return indexA - indexB;
+    }
+    private void swap(char[] array, int i, int j) {
+        char temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
